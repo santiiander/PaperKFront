50d082b803b846642cea5a263e366b4e07c022bb
santii_ander <santiagoandermatten1@gmail.com>
2024-09-11 10:47:10 -0300

    service change

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,10 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open('static-cache').then(function(cache) {
         return cache.addAll([
-          '/',
-          'main.css',
-          'main.js',
-          '/index.html'
+          './',
+          './main.css',
+          './main.js',
+          './index.html'
         ]);
       })
     );
