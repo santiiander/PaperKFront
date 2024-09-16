f698c123de75e3127c8569be670c5ddc95934612
santii_ander <santiagoandermatten1@gmail.com>
2024-09-16 00:39:26 -0300

    ojitoContraseña

--- login.js
+++ login.js
@@ -107,3 +107,17 @@ fetch('https://proyectpaperk-production.up.railway.app/firebase-config')
     .catch(error => {
         console.error('Error fetching Firebase config:', error);
     });
+
+document.getElementById('togglePassword').addEventListener('click', function (e) {
+    const passwordField = document.getElementById('password');
+    const icon = e.target;
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+    } else {
+        passwordField.type = 'password';
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+    }
+});
